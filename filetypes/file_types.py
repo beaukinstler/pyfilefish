da77eca375115a97562e789f1647bc000bb3fc3a
beaukinstler <git@beaukinstler.info>
2019-04-09 22:16:03 -0400

    update file types

--- filetypes/file_types.py
+++ filetypes/file_types.py
@@ -26,24 +26,50 @@ class FileProperySet(object):
         # add defaults
         file_properties = self.file_properties
         self.add([
-            file_properties('png', 1000000),
-            file_properties('iso', 100000000),
-            file_properties('bmp', 1000000),
-            file_properties('gif', 1000000),
-            file_properties('jpg', 1000000),
-            file_properties('jpeg', 1000000),
-            file_properties('mp3', 100000),
-            file_properties('wav', 10000000),
-            file_properties('mp4', 1000000),
-            file_properties('vhd', 500000000),
-            file_properties('vdi', 500000000),
-            file_properties('img', 500000000),
-            file_properties('deb', 500000000),
-            file_properties('msi', 500000000),
-            file_properties('pkg', 500000000),
-            file_properties('app', 500000000),
-            file_properties('oog', 1000000),
-            file_properties('flac', 1000000),
+            file_properties('png', 100),
+            file_properties('bmp', 100),
+            file_properties('gif', 100),
+            file_properties('jpg', 100),
+            file_properties('jpeg', 100),
+
+
+            file_properties('mp3', 100),
+            file_properties('wav', 10000),
+            file_properties('aif', 1000),
+            file_properties('iff', 1000),
+            file_properties('m3u', 1024),
+            file_properties('mpa', 1000),
+            file_properties('m4a', 1000),
+            file_properties('wma', 1000),
+            file_properties('oog', 1000),
+            file_properties('flac', 1000),
+            
+            file_properties('mp4', 1000),
+            file_properties('mov', 1000),
+            file_properties('mpg', 1000),
+            file_properties('rm', 1000),
+            file_properties('3g2', 1000),
+            file_properties('3gp', 1000),
+            file_properties('asf', 1000),
+            file_properties('avi', 1000),
+            file_properties('flv', 1000),
+            file_properties('m4v', 1000),
+            file_properties('srt', 1000),
+            file_properties('swf', 1000),
+            file_properties('vob', 1000),
+            file_properties('wmv', 1000),
+            
+            file_properties('m4v', 1000),
+
+            # file_properties('iso', 100000),
+            # file_properties('vhd', 500000000),
+            # file_properties('vdi', 500000000),
+            # file_properties('img', 500000000),
+            # file_properties('deb', 500000000),
+            # file_properties('msi', 500000000),
+            # file_properties('pkg', 500000000),
+            # file_properties('app', 500000000),
+
         ])
     
     def get_all(self):
