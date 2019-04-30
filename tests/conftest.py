fa8a52fa3d825e1ff547cbfdf5d1a57ca6a69d86
Beau Kinstler <git@beaukinstler.info>
2019-04-30 10:38:56 -0400

    test: update to have more data and new fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,3 +12,13 @@ def file_list():
     # import pdb; pdb.set_trace()
     result = pfu._load_saved_file_list('tests/test_json_data.json')
     return result
+
+@pytest.fixture()
+def file_list_only_one_volume():
+    """primary data set and structure used in pyfi, at least in
+    active time. Basically a dict, loaded and saved to and from json
+    files
+    """
+    # import pdb; pdb.set_trace()
+    result = pfu._load_saved_file_list('tests/test_json_data_one_vol.json')
+    return result
