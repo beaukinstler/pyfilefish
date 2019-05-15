93d2a3a2affb2d9171f3d6eeb49e36360f4b75f3
Beau Kinstler <git@beaukinstler.info>
2019-05-15 13:08:03 -0400

    tests: add tests for crypto and compression...
    and rearrange the tests a little bit

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -1,15 +1,18 @@
 import pytest
 from conftest import pui
+import sys
 
+@pytest.mark.skipif(sys.platform == "linux", reason="does not run on linux")
 def test_prompt_windows_folder():
     ui_folder = pui.prompt_windows_folder(drive_ui="c")
     assert ui_folder == 'c:\\'
-
+            
+@pytest.mark.skipif(sys.platform == "linux", reason="does not run on linux")
 def test_prompt_windows_folder_with_sub():
     ui_folder = pui.prompt_windows_folder(drive_ui="c", folder_ui='Users')
     assert ui_folder == 'c:\\Users'
 
-
+@pytest.mark.skipif(sys.platform == "linux", reason="does not run on linux")
 def test_prompt_windows_folder_throw_error_bad_drive():
     with pytest.raises(ValueError):
         ui_folder = pui.prompt_windows_folder(drive_ui="e", folder_ui='Users')
